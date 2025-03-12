7230079e9b452c25663ffdfae9a612d0f7639dbd
Shiva0909122 <shivarth.drona@gmail.com>
2025-03-12 08:08:20 +0530

    code

--- src/services/api.js
+++ src/services/api.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/api";
+const API_URL = "https://network-marketing.onrender.com/api";
 
 export const getMembers = async () => {
     try {
-        const res = await fetch("http://localhost:5000/api/members");
+        const res = await fetch("https://network-marketing.onrender.com/api/members");
         if (!res.ok) throw new Error("API call failed");
         return await res.json();
     } catch (err) {
